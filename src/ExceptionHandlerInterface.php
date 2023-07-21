57d1276e4b4ef64522ee0f62fd8be565bf4dbf58
Jon Cram <webignition@gmail.com>
2023-07-21 10:58:31 +0100

    Provide event envelope to handlers

--- src/ExceptionHandlerInterface.php
+++ src/ExceptionHandlerInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace SmartAssert\WorkerMessageFailedEventBundle;
 
+use Symfony\Component\Messenger\Envelope;
+
 interface ExceptionHandlerInterface
 {
-    public function handle(\Throwable $throwable): void;
+    public function handle(Envelope $envelope, \Throwable $throwable): void;
 }
